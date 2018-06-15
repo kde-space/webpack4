99272d968ceb891cd20e9358603e539a217c901d
koji.kobayashi <kojikobayashi1840@gmail.com>
2018-06-15 22:55:42 +0900

    add react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div>
+    <p>react here!</p>
+  </div>
+);
+
+export default App;
+
+render(
+  <App />,
+  document.getElementById('app')
+);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello, webpack');
+import App from "./App";
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
