f4ae6c29458a6489ad426d17cbe3d0aaaa13ddeb
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-02-18 02:40:58 +0900

    output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack入門 vol.3',
+      filename: 'webpack.html',
+      template: './src/index.html',
+      description: 'Webpack入門 vol.3 についてのページです'
+    }),
+    new CleanWebpackPlugin(['dist']),
+  ],
 };
