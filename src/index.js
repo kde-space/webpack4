1c1358ac5638c402f41f1eac774e6e344556812d
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-02-18 01:07:16 +0900

    assets management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import xmlData from './data.xml';
+import jsonData from './data.json';
 
 function component() {
   let element = document.createElement('div');
@@ -10,6 +12,10 @@ function component() {
   const myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
+
+  console.log('xmlData :', xmlData);
+  console.log('jsonData :', jsonData);
+  
   return element;
 }
 
