4bc3a65c80d933b735a9e9494e6a1ed1ed832dd4
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-02-18 01:27:11 +0900

    次用の不要コード削除

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import xmlData from './data.xml';
-import jsonData from './data.json';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  console.log('xmlData :', xmlData);
-  console.log('jsonData :', jsonData);
-  
   return element;
 }
 
