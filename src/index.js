6869be21f4f20015f3d034d70fa67c7f1fd9722d
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-02-26 21:31:58 +0900

    production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { cube } from './math.js';
 import { foo } from './expo';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+console.log(hoge);
+
+console.log('process.env.NODE_ENV :', process.env.NODE_ENV);
+
 function component() {
   var element = document.createElement('pre');
   console.log(foo);
