17bd165427f21d59c9bd77f9a39250c6a171c057
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-03-02 22:50:21 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import _ from 'lodash';
-// import print from './print';
+import print from './print';
 
 function component() {
   const element = document.createElement('div');
   const button = document.createElement('button');
   const br = document.createElement('br');
 
-  button.innerHTML = 'Click me and look at the console!';
+  button.innerHTML = 'Click me and look at the console!!';
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.appendChild(br);
   element.appendChild(button);
 
-  // button.onclick = print.bind(null, 'Hello, webpack!!');
+  button.onclick = print.bind(null, 'Hello, webpack!!');
 
   return element;
 }
