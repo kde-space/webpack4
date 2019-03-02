60f4f21df22c47ab49f9aaaf1bc71afb9979c40e
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-03-03 00:13:42 +0900

    整理

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import _ from 'lodash';
-import print from './print';
 
 function component() {
   const element = document.createElement('div');
-  const button = document.createElement('button');
-  const br = document.createElement('br');
-
-  button.innerHTML = 'Click me and look at the console!!';
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(br);
-  element.appendChild(button);
-
-  button.onclick = print.bind(null, 'Hello, webpack!!');
-
   return element;
 }
 
